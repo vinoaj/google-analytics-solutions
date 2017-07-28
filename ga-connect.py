8420bafdcd55432573ca045383b6eece9ec1452e
vinoaj <vinoaj@gmail.com>
2017-07-28 16:48:06 +0800

    refactoring

--- ga-connect.py
+++ ga-connect.py
@@ -11,23 +11,14 @@ SCOPES = [
     'https://www.googleapis.com/auth/analytics.edit',
     'https://www.googleapis.com/auth/analytics.manage.users'
 ]
-DISCOVERY_URI = ('https://analyticsreporting.googleapis.com/$discovery/rest')
+# DISCOVERY_URI = ('https://analyticsreporting.googleapis.com/$discovery/rest')
 CLIENT_SECRETS_PATH = 'client_secrets.json'
 FILE_STORAGE_PATH = 'analyticsreporting.dat'
 VIEW_ID = '<REPLACE_WITH_VIEW_ID>'
 
 
-def initialize_analyticsreporting():
-    """Initializes the analyticsreporting service object.
-
-    Returns:
-    analytics an authorized analyticsreporting service object.
-    """
-    # Parse command-line arguments.
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        parents=[tools.argparser])
-    flags = parser.parse_args([])
+def initialize_analytics_service(api_version=4):
+    flags = get_flags()
 
     # Set up a Flow object to be used if we need to authenticate.
     flow = client.flow_from_clientsecrets(
@@ -43,21 +34,61 @@ def initialize_analyticsreporting():
     if credentials is None or credentials.invalid:
         credentials = tools.run_flow(flow, storage, flags)
 
+    api_version_str = 'v' + str(api_version)
     http = credentials.authorize(http=httplib2.Http())
 
     # Build the service object.
-    analytics = build('analytics', 'v4', http=http, discoveryServiceUrl=DISCOVERY_URI)
+    analytics = build('analytics', api_version_str, http=http)
 
     return analytics
 
 
+"""
+def initialize_analytics_service_v3():
+    flags = get_flags()
+
+    # Set up a Flow object to be used if we need to authenticate.
+    flow = client.flow_from_clientsecrets(
+        client_secrets_path, scope=scope,
+        message=tools.message_if_missing(client_secrets_path))
+
+    # Prepare credentials, and authorize HTTP object with them.
+    # If the credentials don't exist or are invalid run through the native client
+    # flow. The Storage object will ensure that if successful the good
+    # credentials will get written back to a file.
+    storage = file.Storage(api_name + '.dat')
+    credentials = storage.get()
+    if credentials is None or credentials.invalid:
+        credentials = tools.run_flow(flow, storage, flags)
+  
+    http = credentials.authorize(http=httplib2.Http())
+
+    # Build the service object.
+    service = build(api_name, api_version, http=http)
+
+    return service
+"""
+
+
+def get_flags():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        parents=[tools.argparser])
+    flags = parser.parse_args([])
+    return flags
+
+
 def list_accounts(analytics):
-    accounts = analytics.management().accounts().list().execute();
+    accounts = analytics.management().accounts().list().execute()
 
     if accounts.get('items'):
-        account = accounts.get('items')[0]
-        account_id = account.get('id')
-        print(account_id)
+        accounts = accounts.get('items')
+
+        for account in accounts:
+            account_id = account.get('id')
+            account_name = account.get('name')
+            print(account_name)
+            print(account_id)
 
 
 def print_response(response):
@@ -87,8 +118,10 @@ def print_response(response):
 
 
 def main():
-    a = initialize_analyticsreporting()
-    list_accounts(a)
+    service_v3 = initialize_analytics_service(3)
+    # service_v4 = initialize_analytics_service()
+    # print(dir(service_v3))
+    list_accounts(service_v3)
 
 
 if __name__ == "__main__":
