9ac1f4c11be2609752e0871008275f1e8f915979
Vinoaj Vijeyakumaar <vinoaj@gmail.com>
2018-02-14 09:41:44 +1100

    refactoring

--- ga-connect.py
+++ ga-connect.py
@@ -12,7 +12,9 @@ SCOPES = [
     'https://www.googleapis.com/auth/analytics.manage.users'
 ]
 # DISCOVERY_URI = ('https://analyticsreporting.googleapis.com/$discovery/rest')
+# For authentication
 CLIENT_SECRETS_PATH = 'client_secrets.json'
+# Credentials are stored in this file
 FILE_STORAGE_PATH = 'analyticsreporting.dat'
 # VIEW_ID = '<REPLACE_WITH_VIEW_ID>'
 
@@ -44,33 +46,6 @@ def initialize_analytics_service(api_version=4):
     analytics = build('analytics', api_version_str, http=http)
 
     return analytics
-
-
-"""
-def initialize_analytics_service_v3():
-    flags = get_flags()
-
-    # Set up a Flow object to be used if we need to authenticate.
-    flow = client.flow_from_clientsecrets(
-        client_secrets_path, scope=scope,
-        message=tools.message_if_missing(client_secrets_path))
-
-    # Prepare credentials, and authorize HTTP object with them.
-    # If the credentials don't exist or are invalid run through the native client
-    # flow. The Storage object will ensure that if successful the good
-    # credentials will get written back to a file.
-    storage = file.Storage(api_name + '.dat')
-    credentials = storage.get()
-    if credentials is None or credentials.invalid:
-        credentials = tools.run_flow(flow, storage, flags)
-  
-    http = credentials.authorize(http=httplib2.Http())
-
-    # Build the service object.
-    service = build(api_name, api_version, http=http)
-
-    return service
-"""
 
 
 def get_accounts(analytics):
