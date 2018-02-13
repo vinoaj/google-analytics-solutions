92426ee5aee0488526282672cbdb82cf9cd44ea8
Vinoaj Vijeyakumaar <vinoaj@gmail.com>
2018-02-14 09:41:33 +1100

    Introduced archived.py for old code - keep here for reference

---
+++ archived.py
@@ -0,0 +1,25 @@
+"""
+def initialize_analytics_service_v3():
+    flags = get_flags()
+
+    # Set up a Flow object to be used if we need to authenticate.
+    flow = client.flow_from_clientsecrets(
+        client_secrets_path, scope=scope,
+        message=tools.message_if_missing(client_secrets_path))
+
+    # Prepare credentials, and authorize HTTP object with them.
+    # If the credentials don't exist or are invalid run through the native client
+    # flow. The Storage object will ensure that if successful the good
+    # credentials will get written back to a file.
+    storage = file.Storage(api_name + '.dat')
+    credentials = storage.get()
+    if credentials is None or credentials.invalid:
+        credentials = tools.run_flow(flow, storage, flags)
+
+    http = credentials.authorize(http=httplib2.Http())
+
+    # Build the service object.
+    service = build(api_name, api_version, http=http)
+
+    return service
+"""
